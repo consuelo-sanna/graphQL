5644d66741f00beeda156ccc252e938bdf537f02
Consuelo Sanna <consu.sanna@gmail.com>
2020-06-12 12:20:25 +0200

    added a mutations type

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,9 +1,32 @@
 const { gql } = require("apollo-server-express");
 
-const totalPosts = () => 42;
+const { posts } = require("../temp");
+
+// queries
+const totalPosts = () => posts.length;
+
+const allPosts = () => posts;
+
+// mutations.
+// per args ovviamente puoi usare object destructur
+const newPost = (parent, args) => {
+  const post = {
+    id: posts.length + 1,
+    title: args.title,
+    description: args.description,
+  };
+  // push the new post
+  posts.push(post);
+
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
--- server/typeDefs/post.js
+++ server/typeDefs/post.js
@@ -9,7 +9,19 @@ const { gql } = require("apollo-server-express");
 // `;
 
 module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+  }
+
   type Query {
     totalPosts: Int!
+    allPosts: [Post!]!
+  }
+
+  # mutations
+  type Mutation {
+    newPost(title: String!, description: String!): Post!
   }
 `;

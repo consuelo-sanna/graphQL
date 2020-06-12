a9530092527f4785cbb4136ffc473ee813442885
Consuelo Sanna <consu.sanna@gmail.com>
2020-06-12 17:52:29 +0200

    starting login and reg form

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {
     console.log("ojadoij");
@@ -9,21 +10,26 @@ const Login = () => {
 
   return (
     <div className="container p-5">
-      <div className="row p-5">
-        <h4>Login</h4>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label>Email Address</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.emai)}
-              className="form-control"
-              placeholder="Enter email"
-            />
-          </div>
-        </form>
-      </div>
+      <h4>Login</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.emai)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          />
+        </div>
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!email || loading}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("ojadoij");
+  };
+
   return (
     <div className="container p-5">
-      <div className="row p-5">
-        <h4>Register</h4>
-      </div>
+      <h4>Register</h4>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.emai)}
+            className="form-control"
+            placeholder="Enter email"
+            disabled={loading}
+          />
+        </div>
+        <button
+          className="btn btn-raised btn-primary"
+          disabled={!email || loading}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };

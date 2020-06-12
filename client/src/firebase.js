4fb7e578207fffa37bea09871f8cb02af7d2923e
Consuelo Sanna <consu.sanna@gmail.com>
2020-06-12 17:41:48 +0200

    hide config file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyB0Gl25c158jw46PTavheFG9MZqGzT8p-A",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "mern-gql.firebaseapp.com",
   // databaseURL: "https://mern-gql.firebaseio.com",
   projectId: "mern-gql",
   storageBucket: "mern-gql.appspot.com",
   // messagingSenderId: "729180290036",
-  appId: "1:729180290036:web:0b57d37027d1905d4dd7a1",
-  measurementId: "G-E6M1R4YPH0",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,28 @@
 import React from "react";
 
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+
+  const handleSubmit = () => {
+    console.log("ojadoij");
+  };
+
   return (
     <div className="container p-5">
       <div className="row p-5">
         <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email Address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.emai)}
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
